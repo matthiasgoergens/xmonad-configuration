575365bddfb009cbfb7e80f9ba726ac3a6ffb6e6
Matthias Goergens <matthias.goergens@gmail.com>
2025-01-08 13:51:29 +0800

    Snapshot

--- xmonad-config-matthias/app/xmonad.hs
+++ xmonad-config-matthias/app/xmonad.hs
@@ -1,42 +1,42 @@
--- import XMonad.Util.Run
-
-import Data.Default (def)
-import Data.Map qualified as M
 import XMonad
-import XMonad qualified as X
+import qualified XMonad as X
+import XMonad.Actions.UpdatePointer ( updatePointer )
+import XMonad.Hooks.DynamicLog
+-- import XMonad.Util.Run
+import XMonad.Layout.NoBorders ( noBorders )
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import Data.Default (def)
+
 -- For additionalKeysP,
 -- but that does not seem to work with AudioRaiseVolume and LowerVolume (yet?)
+import XMonad.Util.EZConfig
 
+import XMonad.Layout.HintedGrid
+import XMonad.Layout.Tabbed
 -- import XMonad.Layout.Accordion
-
+import XMonad.Layout.MosaicAlt
 -- import XMonad.Layout.HintedTile hiding (Tall)
 -- import XMonad.Layout.Spiral
 import XMonad.Actions.SinkAll (sinkAll)
-import XMonad.Actions.UpdatePointer (updatePointer)
-import XMonad.Hooks.DynamicLog
+
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Layout.HintedGrid
-import XMonad.Layout.MosaicAlt
-import XMonad.Layout.NoBorders (noBorders)
-import XMonad.Layout.Tabbed
-import XMonad.StackSet qualified as W
-import XMonad.Util.EZConfig
 import XMonad.Util.Hacks (fixSteamFlicker)
 
 -- import XMonad.Actions.WithAll
 
 modm = mod4Mask
 
--- modm = XMonad.modMask
+--modm = XMonad.modMask
 
-myLayout = noBorders Full ||| MosaicAlt M.empty ||| noBorders (tabbed shrinkText def) ||| tiled ||| Mirror tiled
-  where
-    tiled = Tall nmaster delta ratio
-    nmaster = 1
-    ratio = 1 / 2
-    delta = 3 / 100
+myLayout = noBorders Full ||| MosaicAlt M.empty ||| noBorders (tabbed shrinkText def) ||| tiled ||| Mirror tiled 
+  where tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 1/2
+        delta = 3/100
 
--- smartBorders (layoutHook defaultConfig)
+             -- smartBorders (layoutHook defaultConfig)
 
 --                           noBorders Full
 --                           ||| smartBorders
@@ -46,47 +46,45 @@ myLayout = noBorders Full ||| MosaicAlt M.empty ||| noBorders (tabbed shrinkText
 -- --                           ||| Mirror (spiral (6/7))
 -- --                           ||| tiled ||| Mirror tiled
 --                           )
-
+             
 -- smartBorders $ layoutHook defaulConfig
 
-defaults =
-  def
-    { terminal = "terminator",
-      -- In xmonad focus follows mouse, this line also makes mouse follow focus:
-      logHook = updatePointer (0.5, 0.5) (0, 0), -- (Relative 0.5 0.5)
-      manageHook = myManageHook,
-      layoutHook = myLayout,
-      -- make mod := left Windows key
-      modMask = mod4Mask,
-      keys = newKeys,
-      handleEventHook =
-        fixSteamFlicker
-        -- handleEventHook def <+> ewmhFullscreen
-    }
-  where
+defaults = def
+           { terminal           = "terminator"
+           -- In xmonad focus follows mouse, this line also makes mouse follow focus:
+           , logHook = updatePointer (0.5, 0.5) (0, 0) -- (Relative 0.5 0.5)
+           , manageHook  = myManageHook
+           , layoutHook = myLayout
+
+          -- make mod := left Windows key
+           , modMask = mod4Mask
+           , keys = newKeys
+           , handleEventHook =
+            fixSteamFlicker
+              -- handleEventHook def <+> ewmhFullscreen 
+           }
     -- default tiling algorithm partitions the screen into two panes
     -- The default number of windows in the master pane
-    tiled = X.Tall nmaster delta ratio
-    nmaster = 1
-    ratio = 1 / 2
-    delta = 3 / 100
+    where tiled   = X.Tall nmaster delta ratio 
+          nmaster    = 1
+          ratio      = 1/2
+          delta      = 3/100
 
 -- pushes all floating windows back into tiling with mod-shift-t:
 myKeys conf@XConfig {XMonad.modMask = modm} =
-  [((modm .|. shiftMask, xK_t), sinkAll)]
-
-newKeys x = M.union (keys def x) (M.fromList (myKeys x))
+    [((modm .|. shiftMask, xK_t), sinkAll)]
+newKeys x  = M.union (keys def x) (M.fromList (myKeys x))
 
 -- Define the workspace an application has to go to
 myManageHook = (className =? "rdesktop" --> doF (W.shift "6")) <+> manageHook def
-
--- composeAll . concat $
+               -- composeAll . concat $
 --            [  -- The applications that float
---   [ className =? i --> doFloat | i <- myClassFloats]
+            --   [ className =? i --> doFloat | i <- myClassFloats]
 --              [
 --            ]
--- 		where
--- 		  myClassFloats	      = ["xvncviewer"]
+--		where
+--		  myClassFloats	      = ["xvncviewer"]
+
 
 -- main = xmonad . ewmh =<< statusBar myBar myPP toggleStrutsKey defaults
 
